3b62f72b61243204185c8ccdb24ad0c1ee92c411
jun <ksh03196@codeit.com>
2025-06-03 22:44:40 +0900

    remote repository test

--- calculator.js
+++ calculator.js
@@ -9,3 +9,11 @@ const subtract = (a, b) => {
 const multiply = (a, b) => {
 	return a * b
 }
+
+const divide = (a, b) => {
+	return a / b
+}
+
+const printHello = (name) => {
+	console.log(`Hello ${name}!`)
+}
